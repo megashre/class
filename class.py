119168cc2520685680d05569f673868eda5dd96b
megashre <73614461+megashre@users.noreply.github.com>
2021-06-02 23:36:57 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,38 @@
+class Atm:
+    def __init__(self,cardnumber,pin):
+        self.cardnumber = cardnumber
+        self.pin = pin
+
+    def check_balance2(self):
+        print("Your balance is 50000")
+
+	def check_balance4(self):
+        print("Your balance is 50000")
+ 
+    def withdrawl(self,amount):
+        new_amount = 50000 - amount
+        print("you have withdrawn amount "+str(amount) +". Your remaining balance is "+ str(new_amount))
+
+
+def main():
+    Card_number = input("insert your card number:- ")
+    pin_number  = input("enter your pin number:- ")
+
+    new_user =  Atm(Card_number ,pin_number)
+
+    print("Choose your activity ")
+    print("1.Balance Enquriy   2.withdrawl")
+    activity = int(input("enter activity number :- "))
+
+    if (activity == 1):
+        new_user.check_balance2()
+		new_user.check_balance4()
+    elif (activity == 2):
+        amount = int(input("enter the amount:- "))
+        new_user.withdrawl(amount)
+    else:
+        print("enter a valid number")
+
+
+
+main()
